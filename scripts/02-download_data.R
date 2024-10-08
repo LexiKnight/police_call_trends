13427a376d73f8b6e261d80ed5a75792bbd001fc
Lexi Knight <lexi.knight@mail.utoronto.ca>
2024-10-07 18:30:31 -0700

    delete unnecessary files and complete the 02-download_data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,31 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto.
+# Author: Lexi Knight
+# Date: 7 October 2024
+# Contact: lexi.knight@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+# Install the necessary packages if needed
+# install.packages("readr")
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+# Load required libraries
+library(readr)
 
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#### Download and save data ####
+# Replace with the direct URL to the CSV file
+csv_url <- "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/police-annual-statistical-report-miscellaneous-calls-for-service/resource/af62a2f5-987b-41e6-944b-272256d25619/download/Miscellaneous%20Calls%20for%20Service.csv"
 
-         
+# Download the CSV file and save it
+download.file(csv_url, destfile = "data/01-raw_data/police_raw_data.csv")
+
+# Read the CSV file into R
+police_raw_data <- read_csv("data/01-raw_data/police_raw_data.csv")
+
+# Print the first few rows of the data as a check
+head(police_raw_data)
+
+
+
